40878764b16d8bcc5f66697f04be92eb3d024054
alone990 <123290292+alone990@users.noreply.github.com>
2023-01-24 22:26:57 +0500

    Update SKB.py

--- SKB.py
+++ SKB.py
@@ -7,7 +7,7 @@ os.system('xdg-open https://facebook.com/groups/3465474777107830/')
 
 bit = platform.architecture()[0]
 if bit == '64bit':
-    import data64
+    import D1
 
 elif bit == '32bit':
     import data32
